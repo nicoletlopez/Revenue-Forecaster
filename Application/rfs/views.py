5448a679c25816f0aa2a689e357e2104a2b105ca
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2017-09-22 13:46:53 +0800

    Added views

--- Application/rfs/views.py
+++ Application/rfs/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.views import generic
+from django.views.generic import View,TemplateView,DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+from django.core.urlresolvers import reverse_lazy
+from .forms import UserForm
+from .models import Project,File
 
-# Create your views here.
+def StartView(request):
+    return redirect('rfs:login')
+
+def LoginView(request):
+    template_name='rfs/login.html'
+    return render(request,template_name)
+
+def IndexView(request):
+
+    projects=Project.objects.all()
+    context={'all_projects':projects}
+    template_name='rfs/base.html'
+    return render(request,template_name,context)
+
+class ProjectDetail(DetailView):
+    model=Project
+    template_name='rfs/project.html'
+
+    def get_context_data(self,**kwargs):
+        context=super(ProjectDetail,self).get_context_data(**kwargs)
+        context['all_projects']=Project.objects.all()
+        return context
+
+class ProjectCreate(CreateView):
+    model=Project
+    template_name='rfs/project_create.html'
+    fields=['project_name']
+    def get_context_data(self,**kwargs):
+        context=super(ProjectCreate,self).get_context_data(**kwargs)
+        context['all_projects']=Project.objects.all()
+        return context
+
