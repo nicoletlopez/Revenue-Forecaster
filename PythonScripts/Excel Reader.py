0a2e536e73200f4721f816acb93bbf3958ed585f
Andrey Bryan M. Buzon <buzonandrey@gmail.com>
2017-09-24 17:34:25 +0800

    Database Query
    It can now connect to a database and execute queries. DB structure is needed to fully complete this Excel Reader

--- PythonScripts/Excel Reader.py
+++ PythonScripts/Excel Reader.py
@@ -2,27 +2,37 @@ from __future__ import print_function
 from os.path import join, dirname, abspath
 import datetime, xlrd
 from xlrd.sheet import ctype_text
+import MySQLdb
+connection=MySQLdb.connect(host="localhost", user="root", passwd="", db="python")
+
+#database syntax
+cur=connection.cursor() #establishes connection
+cur.execute("create table lfy(ID int NOT NULL AUTO_INCREMENT, LastName varchar(255) NOT NULL,FirstName varchar(255),Age int, PRIMARY KEY (ID))") #create Table
+cur.execute("insert into lfy (FirstName,LastName) values('Lars','Monsen')") #Insert to Table
+cur.execute("select * from lfy") #Select from the table
+multiplerow=cur.fetchall() #fetch all of the results of a query and return in a nested list form
+cur.execute("select * from lfy") #must query again since it was the result was already fetched
+row=cur.fetchone() #fetch one row at a time
+connection.commit() #pushes the values to database
+cur.close()
+connection.close()
+
 
 # Open the workbook
-xl_workbook = xlrd.open_workbook('sample.xlsx')
+xl_workbook = xlrd.open_workbook('sample.xlsx') #location and name of the file
 
 # List sheet names, and pull a sheet by name
-#
 sheet_names = xl_workbook.sheet_names()
 print('Sheet Names', sheet_names)
-
 xl_sheet = xl_workbook.sheet_by_name(sheet_names[0])
 
 # Or grab the first sheet by index
-#  (sheets are zero-indexed)
-#
 xl_sheet = xl_workbook.sheet_by_index(4)
 print ('Sheet name: %s' % xl_sheet.name)
 
-# Pull the first row by index
-#  (rows/columns are also zero-indexed)
-#
-row = xl_sheet.row(4)  # 1st row
+# Pull the first row by index(rows/columns are also zero-indexed)
+row = xl_sheet.row(4)
+
 month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 print('(Column #) type:value')
 
@@ -33,6 +43,7 @@ for idx, cell_obj in enumerate(row):
         print('(%s) %s' %(idx, cell_obj.value)) #Prints Subsegment
         mon = 5
         monx = 0
+        cur.execute("create table if NOT EXISTS cell_obj.value(ID int NOT NULL AUTO_INCREMENT, name varchar(255), author varchar(40))")
         for xmonth in month:
             print (xmonth) #prints the Month
             mon = mon + monx
