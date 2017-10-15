4d2d095470405ea5854135f0fa4abe62a9a57fd1
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2017-10-15 18:20:43 +0800

    Restored files because of unintentional commit
    This will be a lesson for committing in master branch. What a headache.

--- Application/rfs/tests.py
+++ Application/rfs/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.views import generic
+from django.views.generic import View,TemplateView,DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse_lazy,reverse
+from .forms import UserForm,FileForm,CreateForm
+from .models import Project,File, Actual, Forecast, Seg_list
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from os.path import join, dirname, abspath
+import datetime, xlrd,numpy as np
+from xlrd.sheet import ctype_text
 
-# Create your tests here.
+def test(request):
+    return render(request,'rfs/data_table.html')
