6405f2f7f3d8ad16b1cdec1cc28321aee66665f5
Andrey Bryan M. Buzon <ambuzon@student.apc.edu.ph>
2017-10-16 13:53:09 +0800

    Minor modifications to django admin
    modifiesd it for easier readablity with potential usability of the end user client

--- Application/rfs/admin.py
+++ Application/rfs/admin.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
 from .models import Project,File, Seg_list, Ind_seg, Grp_seg, Actual, Forecast
 
-admin.site.register(Project)
-admin.site.register(File)
-admin.site.register(Seg_list)
+class ProjectList(admin.ModelAdmin):
+    list_display = ('project_name', 'description', 'status')
+
+class FileList(admin.ModelAdmin):
+    list_display = ('project', 'status')
+    list_filter = ('project', 'status')
+
+class Seg_listList(admin.ModelAdmin):
+    list_display = ('name', 'seg_type')
+    list_filter = ('seg_type', 'name')
+
+class ActualList(admin.ModelAdmin):
+    list_display = ('segment', 'project', 'date', 'actual_id')
+    list_filter = ('segment', 'project')
+
+class ForecastList(admin.ModelAdmin):
+    list_display = ('forecast_id', 'date')
+
+
+admin.site.register(Project, ProjectList)
+admin.site.register(File, FileList)
+admin.site.register(Seg_list, Seg_listList)
 admin.site.register(Ind_seg)
 admin.site.register(Grp_seg)
-admin.site.register(Actual)
-admin.site.register(Forecast)
+admin.site.register(Actual, ActualList)
+admin.site.register(Forecast, ForecastList)
+
+admin.site.site_header = 'Django Admin'
+admin.site.index_title = 'Revenue Forecasting System'
