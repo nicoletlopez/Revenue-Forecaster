f378056b46b1ccf8209ee64d21af061498c5aec0
Andrey Bryan M. Buzon <ambuzon@student.apc.edu.ph>
2017-11-05 19:22:26 +0800

    Added color to Template
    Budget row is now red font to indicate forecasted values

--- PythonScripts/Excel Writer/ExcelWriter.py
+++ PythonScripts/Excel Writer/ExcelWriter.py
@@ -1,9 +1,37 @@
-import pandas
 from openpyxl import load_workbook
+from openpyxl.styles import Font, Color
+from openpyxl.styles import colors
+#Templaate Name
+template = load_workbook('Default.xlsx')
 
-book = load_workbook('2015 Rooms Segmentation.xlsx')
-writer = pandas.ExcelWriter('2016 Rooms Segmentation with Budget Forecast.xlsx', engine='openpyxl')
+#Sheet Name
+SheetName = template['YR2015']
 
-writer.write_cells('9D', 200)
+#Project Name
+SheetName['B1'] = 'Project Name'
 
-writer.save()
+#Project Year
+SheetName['B2'] = 'Jan-Dec, 2017'
+
+#Forecast for what year? Tentative Value is 2017
+yearForecast = 2017
+
+#Change Dates in Excel for Excel Output
+Actualyear = 'Actual ' + str(yearForecast) #Actual Year
+PreviousYear = 'Actual ' + str(yearForecast-1) #Previous Year Actual
+
+#Insert Values to their designated row/column for Excel Output Report
+for i in range(8,64, 4):
+    SheetName.cell(row=i, column=3, value=Actualyear)
+for i in range(10, 66, 4):
+    SheetName.cell(row=i, column=3, value=PreviousYear)
+
+#Budget Row has color red font
+for x in range(9, 65, 4):
+    for y in range(4,73):
+        a = SheetName.cell(row=x, column=y)
+        a.font = Font(color="ff0000")
+
+SheetName['D9'] = 600
+#Excel File Output with Forecast
+template.save(str(yearForecast) + ' Room Segmentation.xlsx')
