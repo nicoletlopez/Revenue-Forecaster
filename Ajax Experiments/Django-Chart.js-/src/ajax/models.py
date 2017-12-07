c83da5c9594d7e547876e4274c22b4ff6d657b94
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2017-12-07 21:32:46 +0800

    Revert "Merge branch 'master' into Andrey"
    This reverts commit d6611a9bb33b63bd1c3672e46ee5b38635924adf, reversing
    changes made to bd1f9ac47d3567b9351649f45f8aee7fefddf68a.

--- Ajax Experiments/Django-Chart.js-/src/ajax/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.core.urlresolvers import reverse
-
-class Actual(models.Model):
-    actual_rns = models.FloatField()
-    actual_arr = models.FloatField()
-    actual_rev = models.FloatField()
