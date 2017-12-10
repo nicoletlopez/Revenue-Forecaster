b6e0618b525dd8186bad7e9729c3e677a0b486f8
jmadriano2 <jmadriano2@users.noreply.github.com>
2017-12-10 22:29:50 +0800

    removed some unnecessary line and added last_year variable

--- PythonScripts/Excel Reader(s)/Excel Reader(Jason).py
+++ PythonScripts/Excel Reader(s)/Excel Reader(Jason).py
@@ -10,13 +10,12 @@ cur = conn.cursor()
 xl_workbook = xlrd.open_workbook('2015 ROOMS SEGMENTATION.xlsx')  # location and name of the file
 sheet_names = xl_workbook.sheet_names()
 xl_sheet = xl_workbook.sheet_by_index(4)
-#print('Sheet name: %s' % xl_sheet.name)
 row = xl_sheet.row(4)
 ind_or_grp = xl_sheet.row(3)
 for iog, cell_obj in enumerate(ind_or_grp):
     if cell_obj.value == 'GROUP':
         group_start = iog
-        #print('%s' % group_start)
+
 ind_actual = np.zeros((13, 12),
                       dtype=[('subsegment', 'S40'), ('month', 'S40'), ('rns', float), ('arr', float), ('rev', float)])
 
@@ -25,16 +24,11 @@ ind_actual_last_year = np.zeros((13, 12),
 
 month_list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
               'November', 'December']
-#print('(Column #) type:value')
 unneeded_columns = ['', 'Barter', 'GRAND TOTAL', 'TOTAL GROUP', 'TOTAL INDIVIDUAL', 'SEGMENT NAME',
                     'Qualified Discount', 'Long Staying']
 
 year = xl_sheet.cell(1,1).value.split()[1]
-#print(year)
-#year_row = year_row.split()
-#year = year_row[1]
-#print(year)
-
+last_year = int(year) - 1
 
 subsegment_column = 0
 m = 0
@@ -45,11 +39,9 @@ for idx, cell_obj in enumerate(row):
     subsegment = cell_obj.value  # use .value to get the value (duh)
 
     if subsegment not in unneeded_columns:  # get the Subsegment (rack, group, etc)
-        #print('(%s) %s' % (idx, subsegment))
         mon = 5
         monx = 0
         for month in month_list:  # get the month (January, February etc)
-            #print(month)
             ind_actual[subsegment_column, m]['subsegment'] = subsegment
             ind_actual_last_year[subsegment_column, m]['subsegment'] = subsegment
             ind_actual[subsegment_column, m]['month'] = month
@@ -65,17 +57,12 @@ for idx, cell_obj in enumerate(row):
                     val = 'rev'
                 our = xl_sheet.cell(actual_row, ecolumn).value
                 our2 = xl_sheet.cell(actual_row_last_year, ecolumn).value
-                #print("our: %s" %our)
-                #print("our2: %s" %our2)
                 if isinstance(our,str):
                     our = 0.0
                 if isinstance(our2,str):
                     our2 = 0.0
-                #print(type(our))
                 ind_actual[subsegment_column, m][val] = our
                 ind_actual_last_year[subsegment_column, m][val] = our2
-                # print(m)
-                # print(erow)
             actual_row += 4
             actual_row_last_year += 4
             m += 1
@@ -83,8 +70,6 @@ for idx, cell_obj in enumerate(row):
         actual_row = 7
         actual_row_last_year = 9
         m = 0
-
-print(ind_actual_last_year)
 
 def getDate(month,year):
     thirty_ones = ["January","March","May","July","August","October","December"]
@@ -113,13 +98,10 @@ for main in ind_actual:
             seg_id_get_query = "select id from seg_list where name like  '%%%s' limit 1" % segment
             cur.execute(seg_id_get_query)
             seg_id = cur.fetchone()[0]
-            #print("%s %s %s %s %s %s" % (date, segment, month, rns, arr, rev))
             insert_query = "insert into actual (date,rns,arr,rev,seg_id) values('%s',%s,%s,%s,%s)" % (
             date, rns, arr, rev, seg_id)
             cur.execute(insert_query)
             conn.commit()
         except(Exception):
-            #print(Exception.__traceback__)
             pass
-    #print()
 conn.close()
